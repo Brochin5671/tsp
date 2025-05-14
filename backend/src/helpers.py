5422ee3e8d3dcbbe7d2c13c09f7ef888adf3a800
Brochin5671 <maximbrochin@gmail.com>
2025-05-14 11:36:18 -0400

    Add a cache version of requests

--- backend/src/helpers.py
+++ backend/src/helpers.py
@@ -1,6 +1,7 @@
 from datetime import datetime, UTC, timedelta
 from pydantic import AwareDatetime
 import requests
+from requests_cache import CachedSession
 from typing import Any, Callable
 
 REQUEST_HEADERS: dict[str, str] = {
@@ -30,6 +31,34 @@ def request_get_json(
     data = None
     try:
         res = requests.get(url, params, headers=headers, timeout=timeout)
+        res.raise_for_status()
+        data = res.json()
+    except requests.exceptions.RequestException as e:
+        data = exception_handler(e)
+    return data
+
+
+def request_get_json_cached(
+        url: str,
+        session: CachedSession,
+        *,
+        params: dict[str, Any] | None = None,
+        exception_handler: Callable[[requests.RequestException], Any] = noop,
+        headers: dict[str, Any] | None = None,
+        timeout: int = 10
+) -> Any:
+    """Handles a GET request and returns the json-encoded content of a response, if any.
+        Args:
+            url (str): URL for the new `Request` object.
+            session (CachedSession): a cached version of a `Session` object for making requests.
+            params (dict[str, Any]): Optional. A list of tuples or bytes to send in the query string for the `Request`.
+            exception_fn (Callable[[RequestException], Any]): Optional. A function that takes in the `RequestException` and returns json-encoded content, if any.
+            headers (dict[str, Any]): Optional. A dictionary of HTTP headers to send to the specified url.
+            timeout (int): Optional. A number indicating how many seconds to wait for the client to make a connection and/or send a response.
+    """
+    data = None
+    try:
+        res = session.get(url, params, headers=headers, timeout=timeout)
         res.raise_for_status()
         data = res.json()
     except requests.exceptions.RequestException as e:
