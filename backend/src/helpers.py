0c2950efcb605e04820c8f363ce45037abf6eba7
Brochin5671 <maximbrochin@gmail.com>
2025-05-17 14:01:04 -0400

    Only catch exceptions if a handler was provided

--- backend/src/helpers.py
+++ backend/src/helpers.py
@@ -43,26 +43,32 @@ def request_get_json_cached(
         session: CachedSession,
         *,
         params: dict[str, Any] | None = None,
-        exception_handler: Callable[[requests.RequestException], Any] = noop,
+        exception_handler: Callable[[
+            requests.RequestException], Any] | None = None,
         headers: dict[str, Any] | None = None,
         timeout: int = 10
 ) -> Any:
-    """Handles a GET request and returns the json-encoded content of a response, if any.
+    '''Handles a GET request and returns the json-encoded content of a response, if any.
         Args:
             url (str): URL for the new `Request` object.
             session (CachedSession): a cached version of a `Session` object for making requests.
             params (dict[str, Any]): Optional. A list of tuples or bytes to send in the query string for the `Request`.
-            exception_fn (Callable[[RequestException], Any]): Optional. A function that takes in the `RequestException` and returns json-encoded content, if any.
+            exception_handler (Callable[[RequestException], Any]): Optional. A function that takes in the `RequestException` and returns json-encoded content, if any.
             headers (dict[str, Any]): Optional. A dictionary of HTTP headers to send to the specified url.
             timeout (int): Optional. A number indicating how many seconds to wait for the client to make a connection and/or send a response.
-    """
+    '''
     data = None
-    try:
+    if exception_handler is not None:
+        try:
+            res = session.get(url, params, headers=headers, timeout=timeout)
+            res.raise_for_status()
+            data = res.json()
+        except requests.exceptions.RequestException as e:
+            data = exception_handler(e)
+    else:
         res = session.get(url, params, headers=headers, timeout=timeout)
         res.raise_for_status()
         data = res.json()
-    except requests.exceptions.RequestException as e:
-        data = exception_handler(e)
     return data
 
 
