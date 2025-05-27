41ea71f55a28bb3f8fce459533bb615a1f83d8f4
Brochin5671 <maximbrochin@gmail.com>
2025-05-27 16:39:14 -0400

    Use CachedSession

--- backend/src/get_articles.py
+++ backend/src/get_articles.py
@@ -2,41 +2,36 @@ from bs4 import BeautifulSoup, ResultSet
 from datetime import datetime
 
 from pydantic import AwareDatetime
+
 from .models import Article
 from dateutil import parser
-import requests
-from .helpers import request_get_json, datetime_UTC, REQUEST_HEADERS
+from .helpers import request_get_json, datetime_UTC, REQUEST_HEADERS, request_get_json_cached
 from itertools import chain
+from requests_cache import CachedSession
 
 
 def get_SNAPI_articles(earliestDatetime: str) -> list[Article]:
     '''Return extracted industry news articles from SNAPI.'''
 
-    # Exception handler for SNAPI call
-    def handle_SNAPI_exception(e):
-        '''Returns an empty dictionary on failure.'''
-        print(e)  # TODO: logging
-        return {}
-
     # Get industry space news articles from SNAPI call
     url = 'https://api.spaceflightnewsapi.net/v4/articles'
     # published_at_gte refers to all documents published after a given ISO8601 timestamp (included)
     params = {'published_at_gte': earliestDatetime, 'limit': 20}
-    results = request_get_json(
-        url, params, exception_handler=handle_SNAPI_exception)
+    with CachedSession() as session:
+        results = request_get_json_cached(url, session, params=params)
 
-    # Paginate through all the results of query
-    items = []
-    if results and 'results' in results:
-        nextURL = results.get('next')
-        while nextURL:
-            # Requesting next data
-            nextResults = request_get_json(nextURL)
-            # Adding to the original results dictionary
-            results['results'] += nextResults['results']
-            # Updating the next URL
-            nextURL = nextResults.get('next')
-        items = results['results']
+        # Paginate through all the results of query
+        items = []
+        if results and 'results' in results:
+            nextURL = results.get('next')
+            while nextURL:
+                # Requesting next data
+                nextResults = request_get_json(nextURL)
+                # Adding to the original results dictionary
+                results['results'] += nextResults['results']
+                # Updating the next URL
+                nextURL = nextResults.get('next')
+            items = results['results']
 
     # Extract data from results
     articles = [Article(title=item['title'],
@@ -54,26 +49,23 @@ def get_SNAPI_articles(earliestDatetime: str) -> list[Article]:
 def get_physorg_articles(earliestDatetime: AwareDatetime) -> list[Article]:
     '''Scrapes phys.org RSS feeds and returns a list of articles.'''
 
-    def get_physorg_items(url: str) -> ResultSet:
+    def _get_physorg_items(url: str, session: CachedSession) -> ResultSet:
         '''Extracts articles from a given phys.org RSS feed.'''
         # Get RSS Feed items
-        items = []
-        try:
-            res = requests.get(url, headers=REQUEST_HEADERS, timeout=10)
-            res.raise_for_status()
-            soup = BeautifulSoup(res.text, 'xml')
-            items = soup.find_all('item')
-        except Exception as e:
-            print(e)  # TODO: logging
+        res = session.get(url, headers=REQUEST_HEADERS, timeout=10)
+        res.raise_for_status()
+        soup = BeautifulSoup(res.text, 'xml')
+        items = soup.find_all('item')
         return items
 
     # Extract articles from RSS feeds
-    astrobiologyItems = get_physorg_items(
-        'https://phys.org/rss-feed/space-news/astrobiology')
-    astronomyItems = get_physorg_items(
-        'https://phys.org/rss-feed/space-news/astronomy')
-    planetarySciItems = get_physorg_items(
-        'https://phys.org/rss-feed/space-news/planetary-sciences')
+    with CachedSession() as session:
+        astrobiologyItems = _get_physorg_items(
+            'https://phys.org/rss-feed/space-news/astrobiology', session)
+        astronomyItems = _get_physorg_items(
+            'https://phys.org/rss-feed/space-news/astronomy', session)
+        planetarySciItems = _get_physorg_items(
+            'https://phys.org/rss-feed/space-news/planetary-sciences', session)
     items = chain(astrobiologyItems, astronomyItems, planetarySciItems)
 
     # Extract data from items
